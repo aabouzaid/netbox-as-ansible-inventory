4b3b8b3dadffeecf1833b1d587487cfc14f6d4ad
Ahmed AbouZaid <ahmed.m@aabouzaid.com>
2017-04-09 13:31:56 +0200

    tidy up

--- tests/test_netbox.py
+++ tests/test_netbox.py
@@ -7,8 +7,8 @@ import responses
 
 # Pathes.
 test_cfg = {
-  "netbox_config": netbox.get_full_path("tests/files/test_netbox.yml"),
-  "api_sample": netbox.get_full_path("tests/files/test_api_output.json")
+    "netbox_config": netbox.get_full_path("tests/files/test_netbox.yml"),
+    "api_sample": netbox.get_full_path("tests/files/test_api_output.json")
 }
 
 # Get Netbox config and API output.
@@ -16,19 +16,20 @@ config = netbox.open_yaml_file(test_cfg["netbox_config"])
 with open(test_cfg["api_sample"]) as data_file:    
     netbox_api_output = data_file.read()
 
+
 # Fake args.
-class args(object):
-    config_file=test_cfg["netbox_config"]
-    host=None
-    list=True
+class Args(object):
+    config_file = test_cfg["netbox_config"]
+    host = None
+    list = True
 
 # Init Netbox class.
-netbox = netbox.NetboxAsInventory(args, config)
+netbox = netbox.NetboxAsInventory(Args, config)
 
 # Fake API response.
 responses.add(responses.GET, netbox.api_url,
-    body=netbox_api_output, status=200,
-    content_type='application/json')
+              body=netbox_api_output, status=200,
+              content_type='application/json')
 
 
 #
@@ -37,5 +38,8 @@ class TestNetboxAsInventory(object):
 
     @responses.activate
     def test_get_hosts_list(self):
+        """
+        Test getting hosts list from API and make sure it returns a list.
+        """
         hosts_list = netbox.get_hosts_list(netbox.api_url)
         assert isinstance(hosts_list, list)
