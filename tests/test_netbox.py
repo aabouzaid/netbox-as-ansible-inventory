04f65c5f13025dbe65a54e5e423eca11248243ce
Ahmed AbouZaid <ahmed.m@aabouzaid.com>
2017-04-12 23:46:45 +0200

    test add_host_to_group function

--- tests/test_netbox.py
+++ tests/test_netbox.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
+import netbox
 import pytest
 import responses
-import netbox
+import json
 
 #
 # Init.
@@ -32,6 +33,54 @@ responses.add(responses.GET, netbox.api_url,
               body=netbox_api_output, status=200,
               content_type='application/json')
 
+# Fake single host.
+fake_host = json.dumps(
+  {
+    "id": 1,
+    "name": "fake_host",
+    "display_name": "Fake Host",
+    "device_type": {
+      "id": 1,
+      "manufacturer": {
+        "id": 8,
+        "name": "Fake Manufacturer",
+        "slug": "fake_manufacturer"
+      },
+      "model": "all",
+      "slug": "all"
+    },
+    "device_role": {
+      "id": 8,
+      "name": "Fake Server",
+      "slug": "fake_server"
+    },
+    "tenant": "null",
+    "platform": "null",
+    "serial": "",
+    "asset_tag": "fake_tag",
+    "rack": {
+      "id": 1,
+      "name": "fake_rack01",
+      "facility_id": "null",
+      "display_name": "Fack Rack01"
+    },
+    "position": "null",
+    "face": "null",
+    "parent_device": "null",
+    "status": "true",
+    "primary_ip": "null",
+    "primary_ip4": "null",
+    "primary_ip6": "null",
+    "comments": "",
+    "custom_fields": {
+      "label": "Web",
+      "env": {
+        "id": 1,
+        "value": "Prod"
+      }
+    }
+  }
+)
 
 #
 # Tests.
@@ -56,3 +105,21 @@ class TestNetboxAsInventory(object):
 
         netbox.add_host_to_group(server_name, group_value, inventory_dict)
         assert server_name in inventory_dict[group_value]
+
+
+    @pytest.mark.parametrize("groups_categories, inventory_dict, host_data", [
+        (
+            {"default": ["device_role", "rack", "platform"]},
+            {"_meta": {"hostvars": {}}},
+            json.loads(fake_host)
+        )
+    ])
+    def test_add_host_to_group(self, groups_categories, inventory_dict, host_data):
+        """
+        Test add host to its group in inventory dict (grouping).
+        """
+
+        netbox.add_host_to_inventory(groups_categories, inventory_dict, host_data)
+        assert "hostvars" in inventory_dict["_meta"]
+        assert "fake_rack01" in inventory_dict
+        assert "fake_host" in inventory_dict["fake_rack01"]
