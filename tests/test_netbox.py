ca90c1ed4ffc8be494fa07695586ff6a1d20746d
Ahmed AbouZaid <ahmed.m@aabouzaid.com>
2017-04-23 23:34:56 +0200

    test get_hosts_list with single host

--- tests/test_netbox.py
+++ tests/test_netbox.py
@@ -13,6 +13,7 @@ test_cfg = commons.test_cfg
 config = commons.config
 netbox_api_output = commons.netbox_api_output
 fake_host = commons.fake_host
+fake_host_json = json.dumps(commons.fake_host)
 
 
 # Fake args.
@@ -23,11 +24,17 @@ class Args(object):
 
 # Init Netbox class.
 netbox_inventory = netbox.NetboxAsInventory(Args, config)
+Args.host = "fake_host"
+netbox_inventory_single = netbox.NetboxAsInventory(Args, config)
 
 
 # Fake Netbox API response.
-def nebox_json_response():
-    commons.fake_json_response(netbox_inventory.api_url, netbox_api_output, 200)
+def netbox_json_response(single_host=False):
+    if single_host:
+        json_payload = fake_host_json
+    else:
+        json_payload = netbox_api_output
+    commons.fake_json_response(netbox_inventory.api_url, json_payload, 200)
 
 
 #
@@ -49,7 +56,7 @@ class TestNetboxAsInventory(object):
         """
         Test get hosts list from API and make sure it returns a list.
         """
-        nebox_json_response()
+        netbox_json_response()
         hosts_list = netbox_inventory.get_hosts_list(netbox_inventory.api_url)
         assert isinstance(hosts_list, list)
 
@@ -57,10 +64,20 @@ class TestNetboxAsInventory(object):
     def test_get_hosts_list_none_url_value(self):
         """
         """
-        nebox_json_response()
+        netbox_json_response()
         with pytest.raises(SystemExit) as none_url_error:
             hosts_list = netbox_inventory.get_hosts_list(None)
         assert none_url_error
+
+    @responses.activate
+    def test_get_hosts_list_single_host(self):
+        """
+        """
+        netbox_json_response(single_host=True)
+        host_data = netbox_inventory_single.get_hosts_list(
+            netbox_inventory_single.api_url,
+            specific_host=netbox_inventory_single.host)
+        assert host_data["name"] == "fake_host"
 
     @pytest.mark.parametrize("server_name, group_value, inventory_dict", [
         ("fake_server", "fake_group", {})
@@ -151,7 +168,7 @@ class TestNetboxAsInventory(object):
         """
         Test generateing final Ansible inventory before convert it to JSON.
         """
-        nebox_json_response()
+        netbox_json_response()
         ansible_inventory = netbox_inventory.generate_inventory()
         assert "fake_host01" in ansible_inventory["_meta"]["hostvars"]
         assert isinstance(ansible_inventory["_meta"]["hostvars"]["fake_host02"], dict)
