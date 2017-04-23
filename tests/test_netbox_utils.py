e86e58f910f49eab8aeda35332595476e053da7e
Ahmed AbouZaid <ahmed.m@aabouzaid.com>
2017-04-23 16:33:16 +0200

    test get_value_by_path cases

--- tests/test_netbox_utils.py
+++ tests/test_netbox_utils.py
@@ -19,12 +19,34 @@ class TestNetboxUtils(object):
 
     @pytest.mark.parametrize("source_dict, key_path", [
         ({"a_key": {"b_key": {"c_key": "c_value"}}},
+         ["a_key", "b_key", "c_key"])
+    ])
+    def test_get_value_by_path_key_exists(self, source_dict, key_path):
+        """
+        """
+        reduced_path = netbox.get_value_by_path(source_dict, key_path)
+        assert reduced_path == "c_value"
+
+    @pytest.mark.parametrize("source_dict, key_path", [
+        ({"a_key": {"b_key": {"c_key": "c_value"}}},
          ["a_key", "b_key", "any"])
     ])
-    def test_get_value_by_path(self, source_dict, key_path):
+    def test_get_value_by_path_key_not_exists(self, source_dict, key_path):
         """
         """
-        reduced_path = netbox.get_value_by_path(source_dict, key_path, ignore_key_error=True)
+        with pytest.raises(SystemExit) as key_not_exists:
+            reduced_path = netbox.get_value_by_path(source_dict, key_path)
+        assert key_not_exists
+
+    @pytest.mark.parametrize("source_dict, key_path, ignore_key_error", [
+        ({"a_key": {"b_key": {"c_key": "c_value"}}},
+         ["a_key", "b_key", "any"],
+         True)
+    ])
+    def test_get_value_by_path_key_not_exists_ignore_error(self, source_dict, key_path, ignore_key_error):
+        """
+        """
+        reduced_path = netbox.get_value_by_path(source_dict, key_path, ignore_key_error)
         assert reduced_path is None
 
     @pytest.mark.parametrize("yaml_file", [
