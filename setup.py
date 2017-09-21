960fd1f6a62a7a05f751d5e6b3b5db425a5568a7
Ahmed AbouZaid <ahmed.m@aabouzaid.com>
2017-09-21 15:56:18 +0200

    add py 2.6

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         'Topic :: System :: Systems Administration',
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
