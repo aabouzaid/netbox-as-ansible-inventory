40d69ab38831a395919ccc483c6c92938098e768
Charlie Root <root@ansible.sweet.local>
2018-02-27 01:47:16 -0600

    Just include the Virtualization stuff

--- netbox/netbox.py
+++ netbox/netbox.py
@@ -81,7 +81,6 @@ class NetboxAsInventory(object):
         self.api_url = self._config(["main", "api_url"])
         self.api_token = self._config(["main", "api_token"], default="", optional=True)
         self.virtualization = self._config(["main", "virtualization"], default=False, optional=True)
-        self.exclude = self._config(["exclude"], default={})
         self.group_by = self._config(["group_by"], default={})
         self.hosts_vars = self._config(["hosts_vars"], default={})
 
@@ -346,19 +345,9 @@ class NetboxAsInventory(object):
             if netbox_hosts_list:
                 for current_host in netbox_hosts_list:
                     server_name = current_host.get("name")
-                    try:
-                        server_role = current_host.get("device_role")["slug"]
-                    except TypeError:
-                        server_role = None
-                    try:
-                        server_platform = current_host.get("platform")["slug"]
-                    except TypeError:
-                        server_platform = None
-                    if not (server_role in self.exclude["role"] or server_platform in self.exclude["platform"]):
-                        self.add_host_to_inventory(self.group_by, inventory_dict, current_host)
-                        host_vars = self.get_host_vars(current_host, self.hosts_vars)
-                        inventory_dict = self.update_host_meta_vars(inventory_dict, server_name, host_vars)
-                        
+                    self.add_host_to_inventory(self.group_by, inventory_dict, current_host)
+                    host_vars = self.get_host_vars(current_host, self.hosts_vars)
+                    inventory_dict = self.update_host_meta_vars(inventory_dict, server_name, host_vars)
         return inventory_dict
 
     def print_inventory_json(self, inventory_dict):
