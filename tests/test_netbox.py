c47ab9f32c045a8f3098f9b3b31090261c51b1be
FragmentedPacket <mikhail.yohman@gmail.com>
2018-08-23 11:56:29 -0600

    Updated test_netbox.py due to formatting reasons

--- tests/test_netbox.py
+++ tests/test_netbox.py
@@ -35,7 +35,6 @@ netbox:
     main:
         api_url: 'http://localhost/api/dcim/devices/'
         api_token: '1234567890987654321234567890987654321'
-
     # How servers will be grouped.
     # If no group specified here, inventory script will return all servers.
     group_by:
@@ -47,7 +46,6 @@ netbox:
         # Custom sections (custom_fields) could be used.
         #custom:
         #    - env
-
     # Use Netbox sections as host variables.
     hosts_vars:
         # Sections related to IPs e.g. "primary_ip" or "primary_ip4".
@@ -61,12 +59,45 @@ netbox:
         #    env: env
 '''
 
+netbox_config_context = '''
+netbox:
+    main:
+        api_url: 'http://localhost/api/dcim/devices/'
+        api_token: '1234567890987654321234567890987654321'
+    # How servers will be grouped.
+    # If no group specified here, inventory script will return all servers.
+    group_by:
+        # Default section in Netbox.
+        default:
+            - device_role
+            - rack
+            - platform
+        # Custom sections (custom_fields) could be used.
+        #custom:
+        #    - env
+    # Use Netbox sections as host variables.
+    hosts_vars:
+        # Sections related to IPs e.g. "primary_ip" or "primary_ip4".
+        ip:
+            ansible_ssh_host: primary_ip
+        # Any other sections.
+        general:
+            rack_name: rack
+        # Custom sections (custom_fields) could be used as vars too.
+        #custom:
+        #    env: env
+        context:
+            config_context: config_context
+'''
+
 # Yaml file with invalid syntax.
 netbox_config_invalid = "invalid yaml syntax: ]["
 
 # Netbox config.
 netbox_config_data = yaml.safe_load(netbox_config)
 
+# Netbox context config.
+netbox_context_config_data = yaml.safe_load(netbox_config_context)
 
 #
 # Netbox api output.
@@ -178,6 +209,68 @@ netbox_api_output = json.loads('''
 }
 ''')
 
+netbox_api_config_context_output = json.loads('''
+{
+  "id": 1,
+  "name": "fake_host01",
+  "display_name": "Fake Host",
+  "device_type": {
+    "id": 1,
+    "manufacturer": {
+      "id": 8,
+      "name": "Fake Manufacturer",
+      "slug": "fake_manufacturer"
+    },
+    "model": "all",
+    "slug": "all"
+  },
+  "device_role": {
+    "id": 8,
+    "name": "Fake Server",
+    "slug": "fake_server"
+  },
+  "tenant": null,
+  "platform": null,
+  "serial": "",
+  "asset_tag": "fake_tag",
+  "rack": {
+    "id": 1,
+    "name": "fake_rack01",
+    "facility_id": null,
+    "display_name": "Fake Rack01"
+  },
+  "position": null,
+  "face": null,
+  "parent_device": null,
+  "status": true,
+  "primary_ip": {
+    "id": 1,
+    "family": 4,
+    "address": "192.168.0.2/32"
+  },
+  "primary_ip4": {
+    "id": 1,
+    "family": 4,
+    "address": "192.168.0.2/32"
+  },
+  "primary_ip6": null,
+  "comments": "",
+  "custom_fields": {
+    "label": "Web",
+    "env": {
+      "id": 1,
+      "value": "Prod"
+    }
+  },
+  "config_context": {
+    "ntp_servers": {
+      "ntp1": "192.168.1.1",
+      "ntp2": "192.168.1.2"
+    }
+  }
+}
+''')
+
 
 # Fake Netbox API response.
 def mock_response(json_payload):
@@ -192,10 +285,16 @@ netbox_api_output_single.update({
     "results": [netbox_api_output["results"][0]]
 })
 fake_host = netbox_api_output["results"][0]
+netbox_output_config_context = netbox_api_output["results"]
 
 # Fake API output.
 netbox_api_all_hosts = mock_response(netbox_api_output)
 netbox_api_single_host = mock_response(netbox_api_output_single)
+netbox_api_config_context = mock_response(netbox_api_config_context_output)
+
+# Fake hosts list for config context tests
+netbox_host_list = []
+netbox_host_list.append(netbox_api_output["results"][0])
 
 
 #
@@ -208,6 +307,7 @@ class Args(object):
 netbox_inventory = netbox.NetboxAsInventory(Args, netbox_config_data)
 Args.list = False
 netbox_inventory_default_args = netbox.NetboxAsInventory(Args, netbox_config_data)
+netbox_inventory_config_context = netbox.NetboxAsInventory(Args, netbox_context_config_data)
 Args.host = "fake_host01"
 netbox_inventory_single = netbox.NetboxAsInventory(Args, netbox_config_data)
 
@@ -318,6 +418,17 @@ class TestNetboxAsInventory(object):
         with pytest.raises(SystemExit) as empty_config_error:
             netbox.NetboxAsInventory(args, config)
         assert empty_config_error
+
+    @pytest.mark.parametrize("netbox_host_list, api_url", [
+        (netbox_host_list, netbox_inventory.api_url)
+    ])
+    def test_get_config_context(self, netbox_host_list, api_url):
+        """
+        Test get config context without token and makes sure it returns a config_context as dict
+        """
+        with patch('requests.get', netbox_api_config_context):
+            config_context_output = netbox_inventory_config_context.get_config_context(netbox_host_list, api_url)
+            assert isinstance(config_context_output[0]['config_context'], dict)
 
     @pytest.mark.parametrize("api_url", [
         (netbox_inventory.api_url)
